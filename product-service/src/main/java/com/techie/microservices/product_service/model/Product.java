1e58a3c8b194acdb2e41e20c14414bb477ef4f77
JAYAKUMARRAMAR <jayakumar92@gmail.com>
2026-06-06 21:56:41 +0530

    updated pom.xml files

--- product-service/src/main/java/com/techie/microservices/product_service/model/Product.java
+++ product-service/src/main/java/com/techie/microservices/product_service/model/Product.java
@@ -1,5 +1,13 @@
 package com.techie.microservices.product_service.model;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import java.math.BigDecimal;
+
 @Document(value = "product")
 @AllArgsConstructor
 @NoArgsConstructor
@@ -7,9 +15,10 @@ package com.techie.microservices.product_service.model;
 @Data
 public class Product {
     @Id
-    private String Id;
+    private String id;
     private String name;
     private String description;
     private BigDecimal price;
 
 }
+
