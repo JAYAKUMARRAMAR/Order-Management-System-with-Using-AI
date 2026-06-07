5e12c6262bb36c8fc0590a0b40d9f9f52d7f3d88
JAYAKUMARRAMAR <jayakumar92@gmail.com>
2026-06-07 17:32:52 +0530

    update product testing

--- product-service/src/test/java/com/techie/microservices/product_service/ProductServiceApplicationTests.java
+++ product-service/src/test/java/com/techie/microservices/product_service/ProductServiceApplicationTests.java
@@ -1,40 +1,55 @@
 package com.techie.microservices.product_service;
 
+
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.context.annotation.Import;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.MongoDBContainer;
 
 import io.restassured.RestAssured;
 
-@Import(TestcontainersConfiguration.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ProductServiceApplicationTests {
 
+
+	@ServiceConnection
+	static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:7.0.5");
+	
 	@LocalServerPort
 	private Integer port;
 
 	@BeforeEach
 	void setUp() {
-		RestAssured.baseURI = "http://localhost:";
+		RestAssured.baseURI = "http://localhost";
 		RestAssured.port = port;
 	}
+	static {
+		mongoDBContainer.start();
+	}
+
 	@Test
 	void shouldCreateProduct() {
 		String requestBody = """
 				{
 					"name": "Test Product",
 					"description": "This is a test product",
-					"price": 19.99
+					"price": 1000
 				}
 				""";
-		RestAssured.given().contentType("application/json").body(requestBody).when().post("/api/products").then().statusCode(201)
-		.body("id", Matchers.notNullValue())
-		.body("name", Matchers.equalTo("Test Product"))
-		.body("description", Matchers.equalTo("This is a test product"))
-		.body("price", Matchers.equalTo(19.99f));
+		RestAssured.given()
+			.contentType("application/JSON")
+			.body(requestBody)
+		.when()
+			.post("/api/products")
+		.then()
+			.statusCode(201)
+			.body("id", Matchers.notNullValue())
+			.body("name", Matchers.equalTo("Test Product"))
+			.body("description", Matchers.equalTo("This is a test product"))
+			.body("price", Matchers.equalTo(1000));
 	}
 
 }
