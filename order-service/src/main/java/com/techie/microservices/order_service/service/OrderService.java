b488c5868dc406dd534aceb1248b2471215aad90
JAYAKUMARRAMAR <jayakumar92@gmail.com>
2026-06-08 20:32:30 +0530

    update order service

--- order-service/src/main/java/com/techie/microservices/order_service/service/OrderService.java
+++ order-service/src/main/java/com/techie/microservices/order_service/service/OrderService.java
@@ -1,29 +1,38 @@
 package com.techie.microservices.order_service.service;
 
-import java.util.UUID;
-
-import org.springframework.stereotype.Service;
-
+import com.techie.microservices.order_service.client.InventoryClient;
 import com.techie.microservices.order_service.dto.OrderRequest;
 import com.techie.microservices.order_service.model.Order;
 import com.techie.microservices.order_service.repository.OrderRepository;
-
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service; 
 
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 public class OrderService {
-    
+
     private final OrderRepository orderRepository;
+    private final InventoryClient inventoryClient;
 
     public void placeOrder(OrderRequest orderRequest) {
-        // Logic to place an order
+        var isProductInStock = inventoryClient.isInStock(orderRequest.skuCode(),orderRequest.quantity());
+
+    if(isProductInStock) {
         Order order = new Order();
         order.setOrderNumber(UUID.randomUUID().toString());
-        order.setPrice(orderRequest.price());
         order.setSkuCode(orderRequest.skuCode());
+        order.setPrice(orderRequest.price());
         order.setQuantity(orderRequest.quantity());
+
         orderRepository.save(order);
+       // log.info("Order placed successfully with order number: {}", order.getOrderNumber());
+    }
+    else{
+        throw new RuntimeException("Product with SKU code " + orderRequest.skuCode() + " is not in stock.");
+    }
     }
 }
+
